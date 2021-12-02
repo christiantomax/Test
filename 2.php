ec7f18934e8e29402791d8505d4fc53586339220
christiantomax <88887827+christiantomax@users.noreply.github.com>
2021-12-02 08:56:32 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,13 @@
+<?php
+	
+	function searchPalindrom(){
+		$text = "di rumah saya ada kasur rusak";
+		$arr = explode(" ",$text);
+		for ($i = 0; $i < count($arr); $i++) {
+		  
+		}
+	}
+	//pseudocode : looping sepanjang character dari string, kemudian didalam looping tersebut melakukan pengecekan apakah huruf awal dan akhir sama, jika sama maka ditampung ke temp result, jika menemukan huruf awal dan akhir yang berbeda maka looping berhenti lanjut ke character string selanjutnya, kemudian melakukan pengecekan kembali character awal dan akhir, jika menemukan huruf awal dan akhir berbeda maka cek result apakah string length lebih besar result lebih besar dari string length temp result, jika iya maka result = temp result, jika tidak maka lanjutkan ke character string berikut nya sampai string length/2
+
+} 
+?>
